a85e74d5c2a205e0d6ec82777e220534f1e5b1a2
zhout <zhout@DESKTOP-FSIIDVA>
2021-02-11 03:37:05 +0200

    Ninth

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,9 +1,9 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,} from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { NavigationExtras, Router } from "@angular/router";
-import { ToastrService } from "ngx-toastr";
-import { Observable, throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 
 @Injectable()
@@ -18,7 +18,7 @@ constructor(private router: Router, private toastr: ToastrService) {
   ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
         catchError(error => {
-            if(error) {
+            if (error) {
                 if (error.status === 400) {
                     if(error.error.errors) {
                         throw error.error;
